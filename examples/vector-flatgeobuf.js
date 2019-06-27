9f3676e82ccfeec42cb0a6dc65d5ab3591f9267c
Björn Harrtell <bjorn@septima.dk>
2019-06-27 22:58:39 +0200

    Streaming deserialization example

--- examples/vector-flatgeobuf.js
+++ examples/vector-flatgeobuf.js
@@ -1,11 +1,19 @@
 import Map from '../src/ol/Map.js';
 import View from '../src/ol/View.js';
-import FlatGeobuf from '../src/ol/format/FlatGeobuf.js';
 import VectorLayer from '../src/ol/layer/Vector.js';
 import VectorSource from '../src/ol/source/Vector.js';
 import {Fill, Stroke, Style, Text} from '../src/ol/style.js';
 
-import {bbox as bboxStrategy} from '../src/ol/loadingstrategy.js';
+import Feature from '../src/ol/Feature.js';
+import Point from '../src/ol/geom/Point.js';
+import MultiPoint from '../src/ol/geom/MultiPoint.js';
+import LineString from '../src/ol/geom/LineString.js';
+import MultiLineString from '../src/ol/geom/MultiLineString.js';
+import Polygon from '../src/ol/geom/Polygon.js';
+import MultiPolygon from '../src/ol/geom/MultiPolygon.js';
+import GeometryLayout from '../src/ol/geom/GeometryLayout.js';
+
+import {all as allStrategy} from '../src/ol/loadingstrategy.js';
 
 const style = new Style({
   fill: new Fill({
@@ -27,16 +35,27 @@ const style = new Style({
   })
 });
 
-const vectorSource = new VectorSource({
-  //url: 'data/flatgeobuf/countries.fgb',
-  url: function(extent) {
-    return 'http://localhost:8080/geoserver/wfs?service=WFS&' +
-        'version=1.1.0&request=GetFeature&typename=topp:states&' +
-        'outputFormat=application/flatgeobuf&srsname=EPSG:4326&' +
-        'bbox=' + extent.join(',') + ',EPSG:4326';
+const ol = {
+  geom: {
+    Point,
+    MultiPoint,
+    LineString,
+    MultiLineString,
+    Polygon,
+    MultiPolygon,
+    GeometryLayout
   },
-  strategy: bboxStrategy,
-  format: new FlatGeobuf()
+  Feature
+};
+
+const vectorSource = new VectorSource({
+  strategy: allStrategy,
+  loader: async function() {
+    const response = await fetch('data/flatgeobuf/countries.fgb')
+    let asyncIterator = flatgeobuf.deserializeStream(response.body, ol);
+    for await (let feature of asyncIterator)
+      this.addFeature(feature);
+  }
 });
 
 const vectorLayer = new VectorLayer({
@@ -52,7 +71,8 @@ const map = new Map({
   target: 'map',
   view: new View({
     center: [0, 0],
-    zoom: 1
+    zoom: 1,
+    projection: 'EPSG:4326'
   })
 });
 
